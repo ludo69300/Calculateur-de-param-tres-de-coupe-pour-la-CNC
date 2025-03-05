e7047d1dd3db88a196b3e2c8592da1f60be3e520
Ludovic Cellard <ludovic.cellard@free.fr>
2025-03-05 13:07:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 function claculeParamcoupe() {
 	let inputMateriau = document.getElementById("typeMateriau");
-	let options = inputMateriau.querySelectorAll("option");
 	
   	indexMateriau = parseInt(inputMateriau.value);
 	console.log(Math.round(4.9));
-	
-    for (let index = 0; index < options.length; index++) {
-        options[index].addEventListener("change", (event) => {
-			indexMateriau = parseInt(event.target.value)
-        })
-    }
+	console.log(Math.round(4.9));
+	 inputMateriau.addEventListener("change", () => {
+  		indexMateriau = inputMateriau.value;
+     console.log(indexMateriau);
+	})
 console.log(indexMateriau);
 }
