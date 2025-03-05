6d245dbcd4b1b4683ed6782183321fcff4b94fb0
Ludovic Cellard <ludovic.cellard@free.fr>
2025-03-05 13:13:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function claculeParamcoupe() {
 	let inputMateriau = document.getElementById("typeMateriau");
+	let inputFraise = document.getElementById("typeFraise");
 	
-  	indexMateriau = parseInt(inputMateriau.value);
+	indexFraise = parseInt(inputFraise.value);
+	indexMateriau = parseInt(inputMateriau.value);
 	console.log(Math.round(4.9));
-	console.log(Math.round(4.9));
+
 	 inputMateriau.addEventListener("change", () => {
-  		indexMateriau = inputMateriau.value;
-     console.log(indexMateriau);
+		indexMateriau = inputMateriau.value;
+	console.log(indexMateriau);
 	})
-console.log(indexMateriau);
+	 inputFraise.addEventListener("change", () => {
+		indexFraise = parseInt(inputFraise.value);
+	console.log(indexFraise);
+	})
 }
